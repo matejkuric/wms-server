5c055589a9d76721faabbe48fe5ef3908928b7c4
matej <matejkuric5@gmail.com>
2018-10-18 11:01:59 +0200

    issa zmenene

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var numb1 = 10;
 var numb2 = 15;
 
-function myFunction(p1, p2){
-    return p1 * p2;
+function myFunction(p1, p2, p3){
+    return p1 * p2 + p3 * p2;
   
 }
 vys = myFunction(numb1, numb2);
 console.log(vys);
+
