eb35415bcf20a508d91ebe879e4c208363468620
matej <matejkuric5@gmail.com>
2018-10-25 10:50:25 +0200

    rrr

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ server.get("/query-test", function(request, response) {
     response.send(request.query);
   });
 
+  server.get("/wms", function(request, response) {
+    var params = request.query;
+    console.log(params);
+
+    if(params.service === 'wms' && params.request === 'GetCapabilities') {
+     response.sendFile(path.join(__dirname , '.\\Capabilities.xml'))
+    } else if(params.service === 'wms' && params.request === 'GetMap') {
+      console.log('idem robit get map')
+    } else {
+      response.send('nepodporovana metoda')
+    }
+  });
+
+
 
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!");
