3262e34d8b0cce430f2fc83962bec5be27b6b235
matej <matejkuric5@gmail.com>
2018-10-25 10:16:55 +0200

    getCapa

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var numb1 = 10;
-var numb2 = 15;
+var express = require("express");
+var server = express();
+var path = require("path");
+var PORT = 3000
 
-function myFunction(p1, p2, p3){
-    return p1 * p2 + p3 * p2;
-  
-}
-vys = myFunction(numb1, numb2);
-console.log(vys);
 
+server.get('/getCapabilities', function (request, response) {
+    console.log(response.sendFile)
+   response.sendFile(path.join(__dirname + '\\Capabilites.xml'))
+   
+})
+
+
+server.get("/query-test", function(request, response) {
+    console.log(request.query);
+    response.send(request.query);
+  });
+
+
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!");
+  });
